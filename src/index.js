d2c6a7868bbfcef0a220dfddffa7e7bbaa0c0d0a
typhonrt <support@typhonrt.org>
2021-06-01 16:30:51 -0700

    add package export

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { run } from '@oclif/core';
+
+/**
+ * Invokes the `testcli-modern` CLI with args programmatically.
+ *
+ * @param {...string} args - args to pass to CLI.
+ *
+ * @returns {Promise<void>}
+ */
+export default async function testcli(...args)
+{
+   return run(args, import.meta.url);
+}
